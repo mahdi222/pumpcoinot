d91ef6730164ba9ed04e8e8134be64b444656bff
mahdi rajabzadeh <37471697+mahdi222@users.noreply.github.com>
2025-08-08 10:32:39 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import asyncio
 from telegram import Bot
+import traceback
 
 import os
 BOT_TOKEN = os.getenv("BOT_TOKEN")
@@ -7,7 +8,12 @@ CHAT_ID = os.getenv("CHAT_ID")
 
 
 async def test():
-    bot = Bot(token=BOT_TOKEN)
-    await bot.send_message(chat_id=CHAT_ID, text="✅ تست موفق! ربات وصل شد.")
+    try:
+        bot = Bot(token=BOT_TOKEN)
+        await bot.send_message(chat_id=CHAT_ID, text="✅ تست موفق! ربات وصل شد.")
+        print("پیام ارسال شد ✅")
+    except Exception as e:
+        print("❌ خطا:")
+        traceback.print_exc()
 
 asyncio.run(test())
